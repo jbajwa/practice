7f55f4f7315783648c31cacedffcf78a60a481af
Jaideep Bajwa <j.bajwa08@gmail.com>
2015-04-07 13:17:09 -0400

    fixed print using parenthesis since is became a function in python 3

--- quickfind.py
+++ quickfind.py
@@ -28,12 +28,12 @@ if __name__ == '__main__':
     qf = QuickFind(10)
     p = [3,4,8,2,5,2,5,7,4,5,0,6]
     q = [4,9,0,3,6,9,9,3,8,6,2,1]
-    print "p q   {} ".format(str(qf.list1)[1:-1])
+    print ("p q   {} ".format(str(qf.list1)[1:-1]))
     for x in range (len(p)):
         if (qf.connected(p[x], q[x])):
             continue
         qf.union(p[x] , q[x])
-        print "{} {}   {} ".format(p[x], q[x], str(qf.list1)[1:-1])
+        print ("{} {}   {} ".format(p[x], q[x], str(qf.list1)[1:-1]))
 
-    print "Component {} ".format(qf.count)
+    print ("Component {} ".format(qf.count))
         
