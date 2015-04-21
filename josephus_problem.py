ca11a6aabaa2dfa17555ba71f86ed73a5d1aabdd
Jaideep Bajwa <j.bajwa08@gmail.com>
2015-04-21 15:53:38 -0400

    josephus problem using circular linked list

---
+++ josephus_problem.py
@@ -0,0 +1,34 @@
+__author__ = 'jaideep'
+class Node:
+    def __init__(self, data):
+        self.value = data
+        self.nextNode = None
+
+if __name__ == '__main__':
+    # creating a circular linked list
+    N = 9
+    M = 5
+    head = Node(1)
+    n = head
+    for i in range (2,N+1):
+        n.nextNode = Node(i)
+        n = n.nextNode
+    n.nextNode = head
+    n = head
+    # printing the list
+    while n.nextNode != head:
+        print (" {} ".format(n.value));
+        n = n.nextNode
+    print (" {} ".format(n.value));
+    # here n points to the last element of the list, where
+    # n.nextNode = head
+    while n != n.nextNode:
+        # jump M-1 element and del the Mth element
+        for i in range(M-1):
+            n = n.nextNode
+        print("removing node {}".format(n.nextNode.value))
+        n.nextNode = n.nextNode.nextNode
+    #last remaining element
+    print ("\nnode left {}".format(n.value))
+
+
